65218965e84822c854797fd6d72050852979d0ef
sengo <vsengo@gmail.com>
2026-04-09 08:56:31 -0400

    login.bash

--- login.bash
+++ login.bash
@@ -2,4 +2,8 @@ appName=velvasam
 keyDir=~/Documents/MyWeb/velvasam2026/velvasam.pem
 IP=184.72.112.22
 USER=bitnami
+<<<<<<< HEAD
 ssh -v -i $keyDir $USER@$IP
+=======
+ssh -v -i $keyDir $USER@$IP
+>>>>>>> 633b2dc (login.bash)
