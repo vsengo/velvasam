4d7560680cf87f67c3c9635b45ef0377cf423f35
sengo <vsengo@gmail.com>
2026-04-10 11:47:21 -0400

    Added install files

--- login.bash
+++
@@ -1,9 +0,0 @@
-appName=velvasam
-keyDir=~/Documents/MyWeb/velvasam2026/velvasam.pem
-IP=184.72.112.22
-USER=bitnami
-<<<<<<< HEAD
-ssh -v -i $keyDir $USER@$IP
-=======
-ssh -v -i $keyDir $USER@$IP
->>>>>>> 633b2dc (login.bash)
